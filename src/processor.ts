42811ac2f07ba37612453e465bc3a4918b20b6d6
dexterslabor <dexterslabor@users.noreply.github.com>
2022-04-22 14:25:20 +0200

    Update processor.ts

--- src/processor.ts
+++ src/processor.ts
@@ -45,77 +45,28 @@ processor.addEventHandler(
     async (ctx: EventHandlerContext) => {
         const transfer = getTransferFromBalancesEvent(ctx);
         const tip = ctx.extrinsic?.tip || 0n;
-        await updatePoolBalance(ctx.store, transfer, tip);
+        // get returns entity or undefined
+        const poolIsSender = await get(ctx.store, Pool, transfer.from);
+        const poolIsReceiver = await get(ctx.store, Pool, transfer.to);
+
+        if (poolIsSender) {
+            // decrease balance
+            let pool = poolIsSender;
+            pool = decreaseBalance(transfer, pool, tip);
+            await ctx.store.save(pool);
+        } else if (poolIsReceiver) {
+            // increase balance
+            let pool = poolIsReceiver;
+            pool = increaseBalance(transfer, pool, tip);
+            await ctx.store.save(pool);
+        } else {
+            // skip because account is not an xyk pool
+            return;
+        }
 
         console.log('balance transfer of ' + transfer.amount + ' BSX');
     }
 );
-
-processor.addEventHandler(
-    'tokens.Transfer',
-    async (ctx: EventHandlerContext) => {
-        const transfer = getTransferFromTokensEvent(ctx);
-        const tip = 0n;
-        await updatePoolBalance(ctx.store, transfer, tip);
-
-        console.log(
-            'token transfer of ' +
-                transfer.amount +
-                ' assetId: ' +
-                transfer.asset
-        );
-    }
-);
-
-processor.addPostHook(async (ctx: BlockHandlerContext) => {
-    const blockHeight = BigInt(ctx.block.height);
-    const createdAt = new Date(ctx.block.timestamp);
-
-    // fetch all pools
-    const pools = await ctx.store.find<Pool>(Pool);
-
-    const databaseQueries = pools.map(async (pool: Pool) => {
-        const historicalBalance = await getOrCreate(
-            ctx.store,
-            HistoricalBalance,
-            `${pool.id}-${blockHeight}`,
-            {
-                assetABalance: pool.assetABalance,
-                assetBBalance: pool.assetBBalance,
-                pool,
-                blockHeight,
-                createdAt,
-            }
-        );
-        return ctx.store.save(historicalBalance);
-    });
-    await Promise.all(databaseQueries);
-});
-
-const updatePoolBalance = async (
-    store: Store,
-    transfer: TransferEventParameters,
-    tip: bigint
-) => {
-    // get returns entity or undefined
-    const poolIsSender = await get(store, Pool, transfer.from);
-    const poolIsReceiver = await get(store, Pool, transfer.to);
-
-    if (poolIsSender) {
-        // decrease balance
-        let pool = poolIsSender;
-        pool = decreaseBalance(transfer, pool, tip);
-        await store.save(pool);
-    } else if (poolIsReceiver) {
-        // increase balance
-        let pool = poolIsReceiver;
-        pool = increaseBalance(transfer, pool, tip);
-        await store.save(pool);
-    } else {
-        // skip because account is not an xyk pool
-        return;
-    }
-};
 
 processor.run();
 
@@ -124,42 +75,6 @@ interface TransferEventParameters {
     to: string;
     amount: bigint;
     asset: bigint;
-}
-
-function increaseBalance(
-    transfer: TransferEventParameters,
-    pool: Pool,
-    tip: bigint
-): Pool {
-    if (transfer.asset === pool.assetA) {
-        pool.assetABalance = pool.assetABalance || 0n;
-        pool.assetABalance += transfer.amount;
-        if (transfer.asset === 0n) pool.assetABalance += tip;
-    }
-    if (transfer.asset === pool.assetB) {
-        pool.assetBBalance = pool.assetBBalance || 0n;
-        pool.assetBBalance += transfer.amount;
-        if (transfer.asset === 0n) pool.assetBBalance += tip;
-    }
-    return pool;
-}
-
-function decreaseBalance(
-    transfer: TransferEventParameters,
-    pool: Pool,
-    tip: bigint
-): Pool {
-    if (transfer.asset === pool.assetA) {
-        pool.assetABalance = pool.assetABalance || 0n;
-        pool.assetABalance -= transfer.amount;
-        if (transfer.asset === 0n) pool.assetABalance -= tip;
-    }
-    if (transfer.asset === pool.assetB) {
-        pool.assetBBalance = pool.assetBBalance || 0n;
-        pool.assetBBalance -= transfer.amount;
-        if (transfer.asset === 0n) pool.assetBBalance -= tip;
-    }
-    return pool;
 }
 
 function getTransferFromBalancesEvent(
@@ -175,15 +90,38 @@ function getTransferFromBalancesEvent(
     };
 }
 
-function getTransferFromTokensEvent(
-    ctx: EventHandlerContext
-): TransferEventParameters {
-    const event = new TokensTransferEvent(ctx);
+function increaseBalance(
+    transfer: TransferEventParameters,
+    pool: Pool,
+    tip: bigint
+): Pool {
+    if (transfer.asset === pool.assetA) {
+        pool.assetABalance = pool.assetABalance || 0n;
+        pool.assetABalance += transfer.amount;
+        pool.assetABalance += tip;
+    }
+    if (transfer.asset === pool.assetB) {
+        pool.assetBBalance = pool.assetBBalance || 0n;
+        pool.assetBBalance += transfer.amount;
+        pool.assetBBalance += tip;
+    }
+    return pool;
+}
 
-    return {
-        from: ss58.codec('basilisk').encode(event.asLatest.from),
-        to: ss58.codec('basilisk').encode(event.asLatest.to),
-        amount: event.asLatest.amount,
-        asset: BigInt(event.asLatest.currencyId),
-    };
+function decreaseBalance(
+    transfer: TransferEventParameters,
+    pool: Pool,
+    tip: bigint
+): Pool {
+    if (transfer.asset === pool.assetA) {
+        pool.assetABalance = pool.assetABalance || 0n;
+        pool.assetABalance -= transfer.amount;
+        pool.assetABalance -= tip;
+    }
+    if (transfer.asset === pool.assetB) {
+        pool.assetBBalance = pool.assetBBalance || 0n;
+        pool.assetBBalance -= transfer.amount;
+        pool.assetBBalance -= tip;
+    }
+    return pool;
 }
